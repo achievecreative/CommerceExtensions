cc830f5f08ab3c1c53bfa61a39d6b50e975447de
Owen chen <owen.chen@blackmores.com.au>
2020-11-24 08:29:07 +0800

    Filter bizfx environment list via Role

---
+++ BizFx/Engine/BizFx/ConfigureSitecore.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigureSitecore.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2020
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Commerce.Core;
+using Sitecore.Framework.Configuration;
+using Sitecore.Framework.Pipelines.Definitions.Extensions;
+using Sitecore.Framework.Rules;
+using System.Reflection;
+using Achievecreative.Commerce.Plugin.BizFx.Pipelines.Blocks;
+using Sitecore.Commerce.Plugin.BusinessUsers;
+using ConfigureServiceApiBlock = Sitecore.Commerce.Core.ConfigureServiceApiBlock;
+
+namespace Achievecreative.Commerce.Plugin.BizFx
+{
+    /// <summary>The configure sitecore class.</summary>
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        /// <summary>The configure services.</summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.RegisterAllPipelineBlocks(assembly);
+            services.RegisterAllCommands(assembly);
+
+            services.Sitecore().Rules(config => config.Registry(registry => registry.RegisterAssembly(assembly)));
+
+            services.Sitecore().Pipelines(builder => builder
+                .ConfigurePipeline<IBizFxEnvironmentsPipeline>(pipeline => pipeline.Add<FilterEnvironmentsByRoleViewBlock>().After<FilterEnvironmentsViewBlock>())
+            );
+        }
+    }
+}
---
+++ BizFx/Engine/BizFx/Pipelines/Blocks/FilterEnvironmentsByRoleViewBlock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.EntityViews;
+using Sitecore.Framework.Pipelines;
+using Sitecore.Framework.Pipelines.Abstractions;
+
+namespace Achievecreative.Commerce.Plugin.BizFx.Pipelines.Blocks
+{
+    public class FilterEnvironmentsByRoleViewBlock : AsyncPipelineBlock<EntityView, EntityView, CommercePipelineExecutionContext>
+    {
+        private const string RolePrefix = "sitecore\\Environment";
+
+        public override Task<EntityView> RunAsync(EntityView arg, CommercePipelineExecutionContext context)
+        {
+            //Environment Name role start with "Environment"
+
+            var roles = context.CommerceContext.Headers["Roles"].ToArray().SelectMany(x => x.Split('|'))
+                .Where(x => x.StartsWith(RolePrefix, StringComparison.OrdinalIgnoreCase)).Select(x =>
+                    x.Replace(RolePrefix, string.Empty, StringComparison.OrdinalIgnoreCase));
+
+            if (roles?.Any() ?? false)
+            {
+                arg.ChildViews.RemoveAll(x => x is EntityView view && !roles.Contains(view.ItemId));
+            }
+
+            return Task.FromResult(arg);
+        }
+    }
+}
